c9e75d6827dc9a0275f1e0fe6e827b619db2f85e
Henrique <henrique.thsantos.ths@gmail.com>
2021-04-28 10:10:50 -0300

    Lista de usurários com botões para edição e delete

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -2,5 +2,11 @@ import React from "react";
 import { Text } from "react-native";
 
 export default (props) => {
-    return <Text>UserForm</Text>;
+    // console.warn(props);
+    const user = props.route.params;
+    return (
+        <Text>
+            {user.name} - {user.email}
+        </Text>
+    );
 };
